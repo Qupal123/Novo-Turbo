d4fbab8203fc7f87b80586509023a4a8573f926a
Qupal123 <grt_qupal@mail.ru>
2026-04-09 00:42:59 +0600

    Add PermissionHelper.java

---
+++ PermissionHelper.java
@@ -0,0 +1,8 @@
+package com.example.novoturbo;
+
+public class PermissionHelper {
+    // Sample implementation for PermissionHelper
+    public void requestPermissions() {
+        // Request permissions logic
+    }
+}
